003358d518230727ec62a715f7d7e033da4a3f72
moo-farah <rizmik.farah@gmail.com>
2026-07-15 15:20:34 +0300

    add updated CRUD APIs

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -24,6 +24,67 @@ const createPost = async (req, res) => {
     }
 }
 
+// Read all posts
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server error", error
+        });
+    }
+}
+
+// Update 
+const updatePost = async (req, res) => {
+    try {
+        // Basic validation to check if the body is empty
+
+        //{name: x, description: y, age: z} -> [name, description, age]
+        // {} = truthy value
+        if(Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                message: "No data provided"
+            });
+        }
+
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body,
+            {new: true})
+            
+            if(!post) return res.status(404).json({
+                message: "Post not found"
+            });
+
+            res.status(200).json({
+                message: "Post updated successfully"
+            })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error", error
+        })
+    }
+}
+
+// Delete the post
+const deletePost = async (req, res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id)
+        if(!deleted) return res.status(404).json({message: "Post not found"})   
+        res.status(200).json({message: "Post deleted successfully"})
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error", error
+        })
+    }
+}
+
+
 export {
-    createPost
+    createPost,
+    getPosts,
+    updatePost,
+    deletePost
+    
 }
