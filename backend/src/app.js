647b1c89206eb52dfb2463c6b379b789f17cc84c
moo-farah <rizmik.farah@gmail.com>
2026-07-15 15:17:24 +0300

    add CRUD APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 // routes import
 import userRouter from './routes/user.route.js'
+import postRouter from './routes/post.route.js'
 
 app.use("/api/v1/users", userRouter);
+app.use('/api/v1/posts', postRouter);
 
 // example route: http://localhost:4000/api/v1/users/register
 
